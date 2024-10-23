43da09a017b2af533079746ecd1c80146565bb3d
adievskiy <dievskiy@mail.ru>
2024-10-23 13:31:51 +0400

    Начал собирать всё воедино в main. Добавлен, но не описан класс User

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,16 @@
+import User.User
+
 fun main() {
-    println("Hello World!")
+    val store1 = RegularStore("Москва").apply {
+        addPhone(RegularPhone("Samsung", 500.0))
+        addPhone(RegularPhone("iPhone", 1000.0))
+    }
+
+    val store2 = RegularStore("Питер").apply {
+        addPhone(RegularPhone("Samsung", 505.0))
+        addPhone(RegularPhone("iPhone", 1005.0))
+    }
+
+    val stores = mutableListOf(store1, store2)
+    val user = User()
 }
